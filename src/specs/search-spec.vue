51aa93570608a73b07cadc68b572b1915986b651
yinshuxun <shuxun.yin@gmail.com>
2017-05-25 21:53:51 +0800

    add mic search page

--- src/specs/search-spec.vue
+++ src/specs/search-spec.vue
@@ -1,8 +1,39 @@
 <template>
-    <search-bar></search-bar>
+    <div>
+        <search-bar></search-bar>
+        <div class="navigator">
+            <div class="nav-left">
+                <text class="nav-text">about</text>
+                <text class="nav-num"> {{num}} </text>
+                <text class="nav-num">result</text>
+            </div>
+            <div class="nav-left">
+                <text>+</text>
+                <text>|</text>
+                <text>+</text>
+            </div>
+        </div>
+    </div>
 </template>
-<style>
-
+<style scoped>
+.navigator{
+    width: 750px;
+    height: 96px;
+    flex-direction: row;
+    padding: 0 20px;
+    border-bottom-style:solid ;
+    border-bottom-width: 1px;
+    border-bottom-color: #dae0e5;
+    align-items: center;
+    justify-content: space-between;
+}
+.nav-left{
+    flex-direction: row;
+}
+.nav-num{
+    width: 433px;
+    height: 36px;
+}
 </style>
 <script>
     import searchBar from "../components/SearchBar.vue"
@@ -10,7 +41,7 @@
     export default{
         data(){
             return {
-
+                num:1655
             }
         },
         components:{
