d1b244c00a768b1e0efdcd63424855787d9520b4
yinshuxun <shuxun.yin@gmail.com>
2017-06-02 17:20:02 +0800

    upgrade serval packages about vue

---
+++ src/components/NavigatorBar.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="navigator">
+        <div class="nav-left">
+            <text class="nav-text">about</text>
+            <text class="nav-num"> {{num}} </text>
+            <text class="nav-text">result</text>
+        </div>
+        <div class="nav-right">
+            <text class="micon nav-right-icon1">&#xe618;</text>
+            <text>|</text>
+            <text class="micon nav-right-icon2">&#xe642;</text>
+        </div>
+    </div>
+</template>
+<style scoped>
+    .navigator {
+        width: 750px;
+        height: 96px;
+        flex-direction: row;
+        padding: 0 20px;
+        border-bottom-style: solid;
+        border-bottom-width: 1px;
+        border-bottom-color: #dae0e5;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .nav-left {
+        flex-direction: row;
+        width: 433px;
+        height: 36px;
+    }
+
+    .nav-right {
+        flex-direction: row;
+        height: 48px;
+        text-align: right;
+    }
+
+    .nav-right-icon1 {
+        margin-right: 30px;
+    }
+
+    .nav-right-icon2 {
+        margin-left: 30px;
+    }
+
+    .nav-text {
+        color: #888;
+        font-size: 30px;
+    }
+
+    .nav-num {
+        color: #e54545;
+        font-size: 30px;
+    }
+</style>
+<script>
+    export default{
+        data(){
+            return {
+                num: 1655
+            }
+        }
+    }
+</script>
--- src/specs/search-spec.vue
+++ src/specs/search-spec.vue
@@ -1,64 +1,17 @@
 <template>
     <div>
         <search-bar></search-bar>
-        <div class="navigator">
-            <div class="nav-left">
-                <text class="nav-text">about</text>
-                <text class="nav-num"> {{num}} </text>
-                <text class="nav-text">result</text>
-            </div>
-            <div class="nav-right">
-                <text class="micon nav-right-icon1">&#xe618;</text>
-                <text>|</text>
-                <text class="micon nav-right-icon2">&#xe642;</text>
-            </div>
-        </div>
+        <navigator-bar></navigator-bar>
     </div>
 </template>
 <style scoped>
-    .navigator {
-        width: 750px;
-        height: 96px;
-        flex-direction: row;
-        padding: 0 20px;
-        border-bottom-style: solid;
-        border-bottom-width: 1px;
-        border-bottom-color: #dae0e5;
-        align-items: center;
-        justify-content: space-between;
-    }
 
-    .nav-left {
-        flex-direction: row;
-        width: 433px;
-        height: 36px;
-    }
-
-    .nav-right {
-        flex-direction: row;
-        height: 48px;
-        text-align: right;
-    }
-
-    .nav-right-icon1 {
-        margin-right: 30px;
-    }
-
-    .nav-right-icon2 {
-        margin-left: 30px;
-    }
-    
-    .nav-text{
-        color: #888;
-        font-size: 30px;
-    }
-    .nav-num{
-        color: #e54545;
-        font-size: 30px;
-    }
 </style>
 <script>
     import searchBar from "../components/SearchBar.vue"
+    import navigatorBar from "../components/NavigatorBar.vue"
+
+    var stream = weex.requireModule("stream")
 
     export default{
         data(){
@@ -67,7 +20,33 @@
             }
         },
         components: {
-            searchBar
+            searchBar,
+            navigatorBar
+        },
+        created(){
+            console.log(1)
+            this.getData(res => {
+                console.log(res.data)
+            })
+        },
+        methods: {
+            getData(callback){
+                return stream.fetch({
+//                    method: 'GET',
+//                    type: 'json',
+//                    url: 'https://api.github.com/repos/alibaba/weex'
+
+                    method : "POST",
+                    headers :{"Content-Type": "application/json"},
+                    type: "json",
+                    url: "https://m.made-in-china.com/search/product",
+                    body: JSON.stringify({
+                        word: "led",
+                        page: "2"
+                    })
+
+                }, callback)
+            }
         }
     }
 </script>
